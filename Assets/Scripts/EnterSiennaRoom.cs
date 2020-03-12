63a9a6eea1401217f66416aa0af76fa539fc7886
Sienna Brown <31221385+siennana@users.noreply.github.com>
2020-03-12 16:37:20 -0700

    images

---
+++ Assets/Scripts/EnterSiennaRoom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterSiennaRoom : MonoBehaviour
+{
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
